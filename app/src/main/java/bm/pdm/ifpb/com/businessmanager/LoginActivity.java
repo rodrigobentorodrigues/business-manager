91f724cd6fa3ee7e9fcc774dfe9f2caac83943f3
Rodrigo Bento <rodrigobentor2014@gmail.com>
2018-05-16 21:58:00 -0300

    Layout da tela de login concluido

--- app/src/main/java/bm/pdm/ifpb/com/businessmanager/LoginActivity.java
+++ app/src/main/java/bm/pdm/ifpb/com/businessmanager/LoginActivity.java
@@ -2,12 +2,22 @@ package bm.pdm.ifpb.com.businessmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Setando os valores para o Spinner
+        this.spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tipo, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spinner.setAdapter(adapter);
     }
 }
