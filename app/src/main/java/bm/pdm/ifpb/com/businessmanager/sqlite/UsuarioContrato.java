8e28e9b4226686294c6a6551ba65a295f1f40bc1
Rodrigo Bento <rodrigobentor2014@gmail.com>
2018-07-27 17:48:06 -0300

    SQLite iniciado

---
+++ app/src/main/java/bm/pdm/ifpb/com/businessmanager/sqlite/UsuarioContrato.java
@@ -0,0 +1,48 @@
+package bm.pdm.ifpb.com.businessmanager.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public class UsuarioContrato extends SQLiteOpenHelper {
+
+    public UsuarioContrato(Context context) {
+        super(context, UsuarioDados.nomeBanco, null, UsuarioDados.versaoBanco);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlCriarTabela);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(sqlExcluirTabela);
+        onCreate(db);
+    }
+
+    public static class UsuarioDados implements BaseColumns {
+        public static final String nomeBanco = "usuario.db";
+        public static final int versaoBanco = 1;
+        public static final String tabela = "usuario";
+        public static final String colunaNome = "nome";
+        public static final String colunaCargo = "cargo";
+        public static final String colunaLogin = "login";
+        public static final String colunaSenha = "senha";
+        public static final String colunaTelefone = "telefone";
+        // public static final String colunaIdEmpresa = "idEmpresa";
+    }
+
+    private static final String sqlCriarTabela =
+            "CREATE TABLE " + UsuarioDados.tabela + " (" +
+                    UsuarioDados._ID + " SERIAL PRIMARY KEY," +
+                    UsuarioDados.colunaNome + " TEXT," +
+                    UsuarioDados.colunaCargo + " TEXT, " +
+                    UsuarioDados.colunaLogin + " TEXT, " +
+                    UsuarioDados.colunaSenha + " TEXT, " +
+                    UsuarioDados.colunaTelefone + " TEXT);";
+    private static final String sqlExcluirTabela =
+            "DROP TABLE IF EXISTS " + UsuarioDados.tabela;
+
+}
---
+++ app/src/main/java/bm/pdm/ifpb/com/businessmanager/sqlite/UsuarioDao.java
@@ -0,0 +1,64 @@
+package bm.pdm.ifpb.com.businessmanager.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bm.pdm.ifpb.com.businessmanager.domains.Usuario;
+
+public class UsuarioDao {
+
+    private UsuarioContrato contrato;
+    private SQLiteDatabase db;
+    private Context contexto;
+
+    public UsuarioDao(Context contexto) {
+        this.contrato = new UsuarioContrato(contexto);
+        this.contexto = contexto;
+    }
+
+    public void inserirUsuario(Usuario usuario){
+        db = contrato.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        long resultado;
+        contentValues.put(UsuarioContrato.UsuarioDados.colunaNome, usuario.getNome());
+        contentValues.put(UsuarioContrato.UsuarioDados.colunaCargo, usuario.getCargo());
+        contentValues.put(UsuarioContrato.UsuarioDados.colunaLogin, usuario.getLogin());
+        contentValues.put(UsuarioContrato.UsuarioDados.colunaSenha, usuario.getSenha());
+        contentValues.put(UsuarioContrato.UsuarioDados.colunaTelefone, usuario.getTelefone());
+        resultado = db.insert(UsuarioContrato.
+                UsuarioDados.tabela, null, contentValues);
+        if(resultado == -1){
+            Toast.makeText(contexto, "Erro ao inserir", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(contexto, "Inserido com sucesso", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public List<Usuario> todosUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        db = contrato.getReadableDatabase();
+        String[] campos = {UsuarioContrato.UsuarioDados.colunaNome, UsuarioContrato.UsuarioDados.colunaCargo, UsuarioContrato.UsuarioDados.colunaLogin,
+                UsuarioContrato.UsuarioDados.colunaSenha, UsuarioContrato.UsuarioDados.colunaTelefone};
+        Cursor cursor = db.query(UsuarioContrato.UsuarioDados.tabela, campos, null,
+                null, null, null, null);
+        Log.i("Cursor", cursor.toString());
+        while(cursor.moveToNext()){
+            Usuario usuario = new Usuario();
+            usuario.setNome(cursor.getString(0));
+            usuario.setCargo(cursor.getString(1));
+            usuario.setLogin(cursor.getString(2));
+            usuario.setSenha(cursor.getString(3));
+            usuario.setTelefone(cursor.getString(4));
+            usuarios.add(usuario);
+        }
+        return usuarios;
+    }
+
+}
